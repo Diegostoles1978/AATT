4150b479910453f7da5a761d5d9c8a2e9bb2de57
DiegoAndresVillamizar <dvillamizar527@gmail.com>
2021-10-03 14:35:21 -0500

    crear nueva entidad

---
+++ AcciTransito.App.Dominio/Entidades/Persona_Acidente.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Diagnostics;
+
+namespace AcciTransito.App.Dominio.Entidades
+{
+
+      public class Personas
+    {
+     public int  id  {get;set;}
+     public int  Cedula  {get;set;}
+     public String NumeroAccidente {get;set;}
+
+
+
+    }
+ }
