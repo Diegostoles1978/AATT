b76d510c97c33034a0adeacbd119843eea2f27c3
Milena <milenabohorquez2@gmail.com>
2021-10-04 16:36:57 -0500

    Update Vehiculo_Accidente.cs

---
+++ AcciTransito.App.Dominio/Entidades/Vehiculo_Accidente.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Diagnostics;
+namespace AcciTransito.App.Dominio.Entidades
+{
+    public class Vehiculo_Accidente
+    {
+     public Accidente id_Accidente {get;set;}
+     public Vehiculo  id_vehiculo {get;set;}
+
+    }
+}
