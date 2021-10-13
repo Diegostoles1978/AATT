3b6a66ac1ab32b3ab744d9ec54db9e4d01c309d4
Diegostoles1978 <diegoarlesvc@hotmail.com>
2021-10-13 14:23:15 -0500

    nueva act

--- AcciTransito.App.Dominio/Entidades/Personas.cs
+++ AcciTransito.App.Dominio/Entidades/Personas.cs
@@ -1,22 +1,37 @@
 using System;
 using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace AcciTransito.App.Dominio.Entidades
 {
 
       public class Personas
+
     {
      public int  id  {get;set;}
-     public String NumeroIdentificacion  {get;set;}
+
+     [Required(ErrorMessage = "Campo Requerido")]
+     [StringLength(6, MinimumLength = 8, ErrorMessage="El documento debe tener min 6 digitos max 8")]
+      public String NumeroIdentificacion  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String Nombres  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String Apellidos  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public DateTime FechaNacimiento {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String Direccion  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String Ciudad  {get;set;}
      public int NumeroCelular {get;set;}
 
+      [Required(ErrorMessage = "Campo Requerido")]
       public int ContactoAlterno  {get;set;}
+      [Required(ErrorMessage = "Campo Requerido")]
+      [RegularExpression("\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*", ErrorMessage = "El formato de correo no es válido")]
       public String  CorreoElectronico {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public Genero  Genero {get;set;}
 
     }
--- AcciTransito.App.Dominio/Entidades/Vehiculos.cs
+++ AcciTransito.App.Dominio/Entidades/Vehiculos.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace AcciTransito.App.Dominio.Entidades
 {
@@ -8,11 +10,18 @@ namespace AcciTransito.App.Dominio.Entidades
     {
 
      public int  id  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String Placa  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
+     [StringLength(6, MinimumLength = 4, ErrorMessage="La placa debe tener min 5 digitos max 6")]
      public String Marca  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String Modelo  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
      public String TipoVehiculo  {get;set;}
+     [Required(ErrorMessage = "Campo Requerido")]
     public String LicenciaTransito {get;set;}
+
     public Personas Persona{get;set;}
     }
  }
